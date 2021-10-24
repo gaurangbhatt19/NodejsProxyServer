0a44cdef93b3dddfc8fdab08f9530032dd5afa60
GBSourceCodes <gaurangbhatt19@gmail.com>
2021-10-25 03:11:00 +0530

    "Node Api"

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express=require("express")
 const app=express()
-
+const rateLimit=require("express-rate-limit")
 require('dotenv').config()
-
 const cors=require("cors")
 
+
+
 const port= process.env.PORT || 3030
+
+// limit api windowMs time, max number of api
+const limiter=rateLimit(
+    {
+        windowMs:10 * 2 * 1000,
+        max:5
+    }
+)
+// middleware
+app.use(limiter)
+app.set("trust proxy",1)
+// Static folder
+app.use(express.static('public'))
 
 app.use(cors())
 
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,30 @@
+const url= require('url')
 const express=require("express")
 const router=express.Router()
 const needle= require('needle')
+const apiCache=require("apicache")
+
+
 // Environment variables
 const API_BASE_URL= process.env.API_BASE_URL
 const API_KEY_NAME=process.env.API_KEY_NAME
 const API_KEY_VALUE=process.env.API_KEY_VALUE
 
-router.route('').get(async (req, res) => {
+// INIT cache
+
+let cache=apiCache.middleware
+
+router.route('').get(cache('2 minutes'),async (req, res) => {
     try{
-     const apiResponse= await needle('get',`${API_BASE_URL}`)
+
+        console.log(url.parse(req.url,true).query)
+
+    const params= new URLSearchParams({
+        [API_KEY_NAME]:API_KEY_VALUE,
+        ...url.parse(req.url,true).query
+        
+    })
+     const apiResponse= await needle('get',`${API_BASE_URL}?${params}`)
      const data =apiResponse.body
 
      res.status(200).json(data)
